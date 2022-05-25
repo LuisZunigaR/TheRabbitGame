5db4c9c805c79188ff465049fc43f4388962e272
Jose Alejandro Cifuentes Aluma <error404.alejo@gmail.com>
2022-05-25 11:43:25 -0500

    Changing the for loop to for each loop to make the code more readable

--- sort.py
+++ sort.py
@@ -5,18 +5,18 @@ def AlphanumericStringSort(sentence):
     tmpnumber = []
     tmppair = []
     tmpodd = []
-    for i in range(0,len(sentence)):
-        if sentence[i].isupper():
-            tmpupper.append(sentence[i])
-        elif sentence[i].islower():
-            tmplower.append(sentence[i])
-        else:
-            # TODO validate if is a number to avoid issues
-            if int(sentence[i]) % 2 == 0:
-               tmppair.append(sentence[i])
+    for character in sentence:
+        if character.isupper():
+            tmpupper.append(character)
+        elif character.islower():
+            tmplower.append(character)
+        elif character.isdigit():
+            if int(character) % 2 == 0:
+               tmppair.append(character)
             else:
-                tmpodd.append(sentence[i])
-
+                tmpodd.append(character)
+        else:
+            raise Exception("It's a not valid character")
     tmpnumber = tmppair + tmpodd    
     sorted_sentence =  tmplower + tmpupper + tmpnumber
     return "".join(sorted_sentence)
