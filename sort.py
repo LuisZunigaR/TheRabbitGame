79c8a77dccd3d4195446612239acc265ecf921d4
Jose Alejandro Cifuentes Aluma <error404.alejo@gmail.com>
2022-05-25 11:32:07 -0500

    Creation of the function sort that has an string as input and the output is sorted following the business rules

---
+++ sort.py
@@ -0,0 +1,25 @@
+def sort(sentence):
+    sorted_sentence = []
+    tmplower = []
+    tmpupper = []
+    tmpnumber = []
+    tmppair = []
+    tmpodd = []
+    for i in range(0,len(sentence)):
+        if sentence[i].isupper():
+            tmpupper.append(sentence[i])
+        elif sentence[i].islower():
+            tmplower.append(sentence[i])
+        else:
+            # TODO validate if is a number to avoid issues
+            if int(sentence[i]) % 2 == 0:
+               tmppair.append(sentence[i])
+            else:
+                tmpodd.append(sentence[i])
+
+    tmpnumber = tmppair + tmpodd    
+    sorted_sentence =  tmplower + tmpupper + tmpnumber
+    return "".join(sorted_sentence)
+
+res = sort("2346asadfsGFAIKSM")
+print(res)
